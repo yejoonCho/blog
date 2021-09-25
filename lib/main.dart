1a9a3b42372494a3d5bc4064301a4e22d03aaf4a
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-09-25 22:07:52 +0900

    Initial commit This project is a blog web app

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  primarySwatch: Colors.blue,
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 45,
+      fontWeight: FontWeight.w800,
+      color: Colors.black,
+    ),
+    headline2: TextStyle(
+      fontSize: 27,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    bodyText2: TextStyle(fontSize: 22, height: 1.4),
+    caption: TextStyle(fontSize: 18, height: 1.4),
+  ),
+  appBarTheme: AppBarTheme(
+    color: Colors.transparent,
+    elevation: 0,
+    iconTheme: IconThemeData(color: Colors.black),
+  ),
+);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: theme,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
